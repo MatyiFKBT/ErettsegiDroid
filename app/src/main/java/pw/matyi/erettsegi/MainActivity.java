cf4b306a75e0d30dfeba0b29dfde468d82a3cecb
Matyi <mmatyi1999@gmail.com>
2018-04-26 22:41:07 +0200

    Downloads folder fix

--- app/src/main/java/pw/matyi/erettsegi/MainActivity.java
+++ app/src/main/java/pw/matyi/erettsegi/MainActivity.java
@@ -8,7 +8,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +25,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.InputStream;
+import java.util.Scanner;
 
 import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     String honap,evszak;
     String szint, szintbetu;
     Switch szintkapcs;
+    String fileName;
     Boolean mpdf;
     DownloadManager downloadManager;
     @Override
@@ -40,41 +46,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        evinput = (EditText)findViewById(R.id.ev);
+        evinput = (EditText) findViewById(R.id.ev);
         permission_check();
 
 
     }
 
-    private void download() {
+    public void download() {
         String ev = evinput.getText().toString();
         spinner = (Spinner)this.findViewById(R.id.spinner);
         String targy = getResources().getStringArray(R.array.targy_values)[spinner.getSelectedItemPosition()];
         String link = "http://dload.oktatas.educatio.hu/erettsegi/feladatok_20"+ev+evszak+"_"+szint+"/"+szintbetu+"_"+targy+"_"+ev+honap+"_fl.pdf";
         String[] linksplit = link.split("/");
-        String fileName = linksplit[linksplit.length-1];
+        fileName = linksplit[linksplit.length-1];
+        printtoast(fileName + " letöltése folyamatban...");
         downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
         Uri uri = Uri.parse(link);
         DownloadManager.Request request = new DownloadManager.Request(uri);
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.setDestinationInExternalPublicDir(DIRECTORY_DOWNLOADS, File.separator + "Erettsegi" + File.separator + fileName);
         Long reference = downloadManager.enqueue(request);
-        printtoast("Feladatlap letöltve.");
-        if(mpdf){
-            String mlink = "http://dload.oktatas.educatio.hu/erettsegi/feladatok_20"+ev+evszak+"_"+szint+"/"+szintbetu+"_"+targy+"_"+ev+honap+"_ut.pdf";
+        //printtoast("Feladatlap letöltve.");
+        if (mpdf) {
+            String mlink = "http://dload.oktatas.educatio.hu/erettsegi/feladatok_20" + ev + evszak + "_" + szint + "/" + szintbetu + "_" + targy + "_" + ev + honap + "_ut.pdf";
             linksplit = mlink.split("/");
             fileName = linksplit[linksplit.length - 1];
-            downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+            downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
             uri = Uri.parse(mlink);
             request = new DownloadManager.Request(uri);
             //downloadManager.enqueue(request);
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
             request.setDestinationInExternalPublicDir(DIRECTORY_DOWNLOADS, File.separator + "Erettsegi" + File.separator + fileName);
             reference = downloadManager.enqueue(request);
-            printtoast("Megoldás letöltve.");
+            //printtoast("Megoldás letöltve.");
             megoldas.setChecked(false);
             mpdf = Boolean.FALSE;
-        }
+        } else if (!mpdf) {
+            /*Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    // Actions to do after 1 second
+                    File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Erettsegi/" + fileName);
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(Uri.fromFile(file), "application/pdf");
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                }
+            }, 3000);
+        */}
     }
     public void printtoast(String szoveg) {
         Toast.makeText(this, szoveg, Toast.LENGTH_SHORT).show();
@@ -113,6 +132,8 @@ public class MainActivity extends AppCompatActivity {
             honap = "okt";
             evszak = "osz";
             printtoast(evszak);
+            majus = (CheckBox) findViewById(R.id.majus);
+            majus.setChecked(false);
         }
     }
 
@@ -136,17 +157,30 @@ public class MainActivity extends AppCompatActivity {
             honap = "maj";
             evszak = "tavasz";
             printtoast(evszak);
+            oktober = (CheckBox) findViewById(R.id.oktober);
+            oktober.setChecked(false);
         }
     }
     public void megoldascheck(View v) {
         megoldas = (CheckBox)v;
+        String buttontext = getString(R.id.button);
         if (megoldas.isChecked()) {
             //Set megoldas;
             mpdf = Boolean.TRUE;
+            //button.setText("Letöltés");
+        } else if (!megoldas.isChecked()) {
+            //set megoldas
+            mpdf = Boolean.FALSE;
+            printtoast("megoldas uncheck");
+            //button.setText(buttontext);
+
         }
     }
     private void initialize() {
         button = (Button)findViewById(R.id.button);
+        mpdf = Boolean.FALSE;
+        szint = "kozep";
+        szintbetu = "k";
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
